61048331692ed0944e75708819fbc144b4ad389d
unknown <scenaristeur@gmail.com>
2018-02-04 23:04:49 +0100

    mon node serveur

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 'use strict';
+///////////////////////////////////////////////////////////
+// GESTION NODE EXPRESS SERVEUR
+///////////////////////////////////////////////////////////
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+//LANCEMENT SERVEUR
+server.listen(port, function () {
+	console.log('Server listening at port %d', port);
+});
+
+/*
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-
+const INDEX = path.join(__dirname, 'index.html');*/
+/*
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
+const io = socketIO(server);*/
 
 io.on('connection', (socket) => {
   console.log('Client connected');
