3f9e9914abafa7415bc1e401b48679293868dc7e
dfaveris <scenaristeur@gmail.com>
2018-05-18 15:44:40 +0200

    label room

--- public/src/spoggy-chat/agents/ChatAgent.js
+++ public/src/spoggy-chat/agents/ChatAgent.js
@@ -36,49 +36,39 @@ ChatAgent.prototype.receive = function(from, message) {
 
 
   if (typeof message == String && message.indexOf('Hello') === 0) {
-    // reply to the greeting
     this.send(from, 'Hi ' + from + ', nice to meet you!');
     this.app.prop1 = message;
   }
 
   switch(message.type){
     case 'updaterooms':
-    console.log(message.rooms);
-    console.log(message.current_room);
-    this.app.$.inputMessage.label = "Mode Collab, room :"+ message.current_room;
-    this.app.$.chat.push('messages',{
+    this.app.room = message.current_room;
+    /*this.app.$.chat.push('messages',{
       author: 'Spoggy',
       text: "CONNEXION A "+message.current_room,
       created: Date.now()
-    });
+    });*/
     break;
-    this.agentDialogs.send('agentChat', {type: "changePseudo", pseudo: this.pseudo});
     case 'changePseudo':
-    console.log('pseudo : '+message.pseudo);
+    //  console.log('pseudo : '+message.pseudo);
     this.app.pseudo = message.pseudo;
     break;
 
     case 'updateChat':
-  //  console.log("username "+message.username);
-  //  console.log(message.data);
-    //  console.log(this.app.socket.username)
-  //  console.log("pseudo "+this.app.pseudo)
-  //  console.log("author avant "+this.app.author)
-    //  this.app.$.inputMessage.label = message.username +" : "+ message.data;
+    console.log(message)
     this.app.$.chat.messages = this.app.messages;
-    //test sur message.username
+    this.room = message.room;
     this.app.author = message.username == this.app.pseudo ? 'me' : 'you'; // For demo
-  //  console.log("author apres "+this.app.author)
     if (this.app.author != 'me'){
       message.data = message.username+": "+message.data;
       if(!this.app.visible){
         this.app.nonlu++;
-          this.app.icon = "";
+        if(this.app.nonlu > 100){
+          this.app.nonlu = "++"
+        }
+        this.app.icon = "";
       }
     }
-    /*if (message.data.length > 40) {
-      message.data = message.data.match(/.{1,40}/g).join("\n")
-    }*/
     this.app.$.chat.push('messages',{
       author: this.app.author,
       text: message.data,
