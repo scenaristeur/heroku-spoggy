ae23627a5dfc5d85b6c0dc7088d200f067eede6b
unknown <scenaristeur@gmail.com>
2018-07-02 23:32:07 +0200

    ajout ports openshift et module fuseki

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var io = require('socket.io')(server);
 
 
 /* CONFIGURATION DU SERVEUR WEB */
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
  ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
