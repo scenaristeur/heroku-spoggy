5604cffba35997ab27ee1eff3bfe52715cbad222
unknown <scenaristeur@gmail.com>
2018-12-02 15:53:41 +0100

    add ide-current

--- gui/src/spoggy-app.js
+++ gui/src/spoggy-app.js
@@ -1,4 +1,5 @@
 import {PolymerElement, html} from 'https://unpkg.com/@polymer/polymer@next/polymer-element.js?module';
+import 'https://cdn.jsdelivr.net/gh/scenaristeur/heroku-spoggy/gui/src/ide-current.js';
 
 class SpoggyApp extends PolymerElement {
 
@@ -8,6 +9,8 @@ class SpoggyApp extends PolymerElement {
     return html`
       <style> .mood { color: green; } </style>
       Web Components SPOGGY are <span class="mood">[[mood]]</span>!
+      <br>
+      <ide-current>ide-current chargement</ide-current>
     `;
   }
 }
